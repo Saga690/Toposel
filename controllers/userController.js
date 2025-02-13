f2631cf20e1333146e9b7f456836ba41331769ad
Saga690 <master.ayush.2003@gmail.com>
2025-02-13 17:31:12 +0530

    minor change

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ export async function searchUser(req, res) {
         if (req.query.username) query.username = req.query.username;
         if (req.query.email) query.email = req.query.email;
 
-        const user = await User.findOne(query).select("-password");
+        const user = await User.findOne(query).select("-password -createdAt -updatedAt -__v");
 
         if (!user) return res.status(404).json({ message: "User not found" });
 
